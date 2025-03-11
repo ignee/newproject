92400a6955311a39c6cc3eef8675f0ecc2f420bd
Madheshvelu Ganesan <madheshvelu.ganesan@truwavesoftware.com>
2025-03-11 15:14:38 +0530

    change made

--- bucks/index.js
+++ bucks/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello from the backend!");
-});
-
-app.listen(5000,console.log("server started on port 5000"));
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello from the backend!" });
+  });
+  
+  module.exports = app;
