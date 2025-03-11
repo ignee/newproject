d1437abdffd7b9e16ba7726e9c7c5646aab034ed
Madheshvelu Ganesan <madheshvelu.ganesan@truwavesoftware.com>
2025-03-11 15:32:50 +0530

    change made

--- bucks/index.js
+++ bucks/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api/hello", (req, res) => {
-    res.json({ message: "Hello from the backend!" });
+    res.json({ message: "Hello this is SAM" });
   });
   
   module.exports = app;
