3c407cf1050e83c112ca730003ecde7d0d9de6fe
Madheshvelu Ganesan <madheshvelu.ganesan@truwavesoftware.com>
2025-03-11 12:38:18 +0530

    Revert "changes made"
    This reverts commit e90fe4915d0e6ff16888840269060ccfd6a04e6e.

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from the backend!" });
-});
-
-module.exports = app;
